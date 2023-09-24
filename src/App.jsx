adeee3a2981bf3d2f5f186c5e9976734e47b653f
Dammy Knight <olaoluwaayomide10@gmail.com>
2023-09-25 00:09:54 +0100

    Adding async function

--- src/App.jsx
+++ src/App.jsx
@@ -55,11 +55,17 @@ const KEY = 'e1992c01';
 export default function App() {
 	const [movies, setMovies] = useState([]);
 	const [watched, setWatched] = useState([]);
+	const query = 'merlin';
 
 	useEffect(function () {
-		fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=merlin`).then((response) =>
-			response.json().then((data) => setMovies(data.Search))
-		);
+		async function fetchMovies() {
+			const res = await fetch(
+				`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+			);
+			const data = await res.json();
+			setMovies(data.Search);
+		}
+		fetchMovies();
 	}, []);
 
 	return (
